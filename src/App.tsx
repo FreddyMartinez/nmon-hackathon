8520cdf7e2766eb2d176bde3f9edb46ced61883c
Freddy Martinez <freddy_hernan.martinez@contractors.roche.com>
2024-02-07 10:43:03 +0100

    chore: add react image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { WidgetList } from './components/widgetList';
-
+import reactLogo from './assets/react.svg'
+import styles from './App.css?inline'
 
 function App() {
  
   return (
     <>
+      <style>{styles}</style>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
       <WidgetList></WidgetList>
     </>
   )
